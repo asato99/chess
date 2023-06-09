b71bc3e878086afa09c040df82dbd350b65079da
asato <asato@sabira.jp>
2023-06-09 13:40:06 +0900

    add update_branch

--- app/models/prospector.py
+++ app/models/prospector.py
@@ -1,9 +1,19 @@
 class Prospector():
     def __init__(self, board, gm):
-        self.prospect_tree = {'prospect': 0.5, 'move': None, 'type': 'opponent', 'branch': []}
+        self.prospect_tree = {'prospect': 0.5, 'move': None, 'side': 'opponent', 'branch': []}
         self.tree_path = []
         self.root_info = {'board': board, 'gm': gm}
 
-    def update_branch(self, moves, type):
-        return
+    def update_branch(self, moves, side):
+        branch = self.prospect_tree['branch']
+        for move in self.tree_path:
+            for tree in branch:
+                if tree['move'] == move:
+                    branch = tree['branch']
+                    break
+        branch = [{'prospect':0.5, 'move':move, 'side':side, 'branch':[]} for move in moves]
+
+    def update_prospect(self):
+        
+
 
